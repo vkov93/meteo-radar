a73809dfb3707084fcace1bb581164f35562f249
vkov93 <vkov93@mail.ru>
2018-03-13 23:11:39 +0300

    first movement for check weather program

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+
+    args = argparse.ArgumentParser()
+    args.add_argument('city_name', type=str)
+    print('Weather in {} +25, sunny'.format(args.parse_args().city_name))
+
 if __name__ == "__main__":
-    print('you can see this only if you open project from main.py') #if i understood correctly
+    main()
