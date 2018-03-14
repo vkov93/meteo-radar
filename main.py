0c7c71a718040c00364e8394a7507747b0250b0c
vkov93 <36864399+vkov93@users.noreply.github.com>
2018-03-14 10:38:13 +0300

    choose the correct name of object - 'parser'
    create new object 'arguments' for call .parse_args only once

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import argparse
 
 def main():
 
-    args = argparse.ArgumentParser()
-    args.add_argument('city_name', type=str)
-    print('Weather in {} +25, sunny'.format(args.parse_args().city_name))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('city_name', type=str)
+
+    arguments = parser.parse_args()
+    print('Weather in {} +25, sunny'.format(arguments.city_name))
 
 if __name__ == "__main__":
     main()
