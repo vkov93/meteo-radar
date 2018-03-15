c5114db1ff88535427393cf2b5c46798454d7cf4
vkov93 <vkov93@mail.ru>
2018-03-16 02:01:23 +0300

    Сarried out the process of obtaining temperature in a separate module

---
+++ mrlibrary.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import sys
+import requests
+
+
+class JsonError(Exception):
+    pass
+
+class HTTPError(Exception):
+    pass
+
+class WeatherProvider:
+
+    def get_woeid(self, city):
+        url = 'https://www.metaweather.com/api/location/search/?query={}'.format(city)
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise HTTPError
+        j = response.json()
+        try:
+            return j[0]['woeid']
+        except:
+            raise JsonError
+
+
+    def temperature_from_woeid(self, woeid):
+        url = 'https://www.metaweather.com/api/location/{}/'.format(woeid)
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise HTTPError
+        j = response.json()
+        try:
+            return j['consolidated_weather'][0]['the_temp']
+        except:
+            raise JsonError
+
+
+    def get_temperature(self, city):
+        woeid = WeatherProvider.get_woeid(self, city)
+        return WeatherProvider.temperature_from_woeid(self, woeid)
