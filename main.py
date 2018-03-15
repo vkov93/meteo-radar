4f7974694b78f301b8d50824649b5cef9093c7f4
vkov93 <vkov93@mail.ru>
2018-03-15 18:54:29 +0300

    add error handling, the program is divided into separate functions

--- main.py
+++ main.py
@@ -2,35 +2,49 @@
 
 import argparse
 import json
+import sys
 import requests
 
+class GetWoeidErr(Exception):
+    pass
+
+class GetTmpErr(Exception):
+    pass
+
+
+def parse_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-c','--city', type=str, required=True, help='Name of the city')
+    return parser.parse_args()
+
+def get_woeid(city):
+    url='https://www.metaweather.com/api/location/search/?query={}'.format(city)
+    response=requests.get(url)
+    if response.status_code != 200:
+        raise Exception
+    j=response.json()
+    try:
+        return j[0]['woeid']
+    except:
+        raise GetWoeidErr
+
+def get_temperature(city):
+    url='https://www.metaweather.com/api/location/{}/'.format(city)
+    response=requests.get(url)
+    if response.status_code != 200:
+        raise Exception
+    j=response.json()
+    try:
+        return j['consolidated_weather'][0]['the_temp']
+    except:
+        raise GetTmpErr
 
 def main():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('city_name', type=str)
-
-    arguments = parser.parse_args()
-    str_sity=str(arguments.city_name)
-
-
-    response = requests.get('https://www.metaweather.com/api/location/search/?query='+str_sity)
-                                                    # knock knock here's johnny
-    to_json = response.json()                       # response to json
-    json_encode = json.dumps(to_json)               # json encoding
-    json_decode = json.loads(json_encode)[0]        # json decoding (obj 'result_dict' type==dict)
-    cityid = json_decode['woeid']                   # get city_id
-
-    response = requests.get('https://www.metaweather.com/api/location/' + str(cityid) + '/')
-                                                    # second encounter
-    to_json = response.json()
-    hemorragelist = to_json['consolidated_weather']
-    # get object type 'list', where the elements are dict
-    temperature = hemorragelist[0]['the_temp']
-    # get object from first list element, then get object with key 'the_temp'
+    arguments=parse_args()
+    woeid=get_woeid(arguments.city)
+    temperature=get_temperature(woeid)
 
     print(temperature)
 
-
-if __name__ == "__main__":
+if __name__=='__main__':
     main()
