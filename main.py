4e3f9bdb3e30f83ed676111fa18fac9e2b5df897
vkov93 <vkov93@mail.ru>
2018-03-15 20:15:42 +0300

    I try in the OOP

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ class GetWoeidErr(Exception):
 class GetTmpErr(Exception):
     pass
 
+class GetUrlErr(Exception):
+    pass
+
 class WeatherProvider():
+    def __new__(cls,city):
+        woeid=WeatherProvider.get_woeid(city)
+        temperature=WeatherProvider.get_temperature(woeid)
+        return temperature
 
     def get_woeid(city):
         url = 'https://www.metaweather.com/api/location/search/?query={}'.format(city)
         response = requests.get(url)
         if response.status_code != 200:
-            raise Exception
+            raise GetUrlErr
         j = response.json()
         try:
             return j[0]['woeid']
@@ -28,24 +35,23 @@ class WeatherProvider():
         url = 'https://www.metaweather.com/api/location/{}/'.format(city)
         response = requests.get(url)
         if response.status_code != 200:
-            raise Exception
+            raise GetUrlErr
         j = response.json()
         try:
             return j['consolidated_weather'][0]['the_temp']
         except:
             raise GetTmpErr
 
-
 def parse_args():
-    parser=argparse.ArgumentParser()
-    parser.add_argument('-c','--city', type=str, required=True, help='Name of the city')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--city', type=str, required=True, help='Name of the city')
     return parser.parse_args()
 
 def main():
-    arguments=parse_args()
-    woeid=WeatherProvider.get_woeid(arguments.city)
-    temperature=WeatherProvider.get_temperature(woeid)
-    print(temperature)
+    args=parse_args()
+    city=args.city
+    a = WeatherProvider(city)
+    print(a)
 
-if __name__=='__main__':
-    main()
+if __name__ == '__main__':
+        main()
