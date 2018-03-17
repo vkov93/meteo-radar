f9c5ff9cb8222adb2514fe2447504058ad5fa6ff
vkov93 <vkov93@mail.ru>
2018-03-17 20:26:26 +0300

    Added new class (DarkSky - for usage DarkSky API) and methods for him Hide print() functions from main.py

--- mrlibrary.py
+++ mrlibrary.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import sys
 import requests
+from cityregistry import CityRegistry
 
 class CityDoesNotExist(Exception):
     pass
@@ -15,6 +16,9 @@ class HTTPError(Exception):
     pass
 
 class MetaWeatherException(Exception):
+    pass
+
+class DarkSkyException(Exception):
     pass
 
 class WeatherProvider:
@@ -54,7 +58,36 @@ class MetaWeather(WeatherProvider):
     def get_temperature(self, city):
         try:
             woeid = self.get_woeid(city)
-            temperature = self.temperature_from_woeid(woeid)
-            return temperature
+            temperature = round(self.temperature_from_woeid(woeid), 3)
+            result = 'Metaweather: Temperature in {} : {}°'.format(city, temperature)
+            print(result)
         except:
             raise MetaWeatherException
+
+
+class DarkSky(WeatherProvider):
+    def __init__(self):
+        super().__init__('DarkSky')
+
+    def temperature_from_ds(self, latitude, longitude):
+        url = 'https://api.darksky.net/forecast/051577d3ef68f75953e9c9a2247582e1/{},{}'\
+            .format(latitude,longitude)
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise HTTPError
+        j = response.json()
+        try:
+            return round((j['currently']['temperature']-32)/1.8, 3)       #Fahrenheit into Celsius
+        except:
+            raise JsonError
+
+    def get_temperature(self, city):
+        try:
+            a = CityRegistry('city_coordinates.json')
+            coordinates = a.get_coordinates(city)
+            temperature = self.temperature_from_ds(coordinates.latitude, coordinates.longitude)
+            result = 'DarkSky: Temperature in {} : {}°'.format(city, temperature)
+            print(result)
+        except:
+            raise DarkSkyException
+
